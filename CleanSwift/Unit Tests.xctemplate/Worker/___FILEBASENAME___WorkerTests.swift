7f17cec4e05966e72f3c769b40764610a66d4c03
Yağızhan Akduman <yagizhanakduman@gmail.com>
2021-10-13 14:26:25 +0300

    unit test scene update

--- CleanSwift/Unit Tests.xctemplate/Worker/___FILEBASENAME___WorkerTests.swift
+++ CleanSwift/Unit Tests.xctemplate/Worker/___FILEBASENAME___WorkerTests.swift
@@ -13,42 +13,35 @@
 @testable import ___PROJECTNAMEASIDENTIFIER___
 import XCTest
 
-class ___VARIABLE_sceneName___WorkerTests: XCTestCase
-{
-  // MARK: Subject under test
-  
-  var sut: ___VARIABLE_sceneName___Worker!
-  
-  // MARK: Test lifecycle
-  
-  override func setUp()
-  {
-    super.setUp()
-    setup___VARIABLE_sceneName___Worker()
-  }
-  
-  override func tearDown()
-  {
-    super.tearDown()
-  }
-  
-  // MARK: Test setup
-  
-  func setup___VARIABLE_sceneName___Worker()
-  {
-    sut = ___VARIABLE_sceneName___Worker()
-  }
-  
-  // MARK: Test doubles
-  
-  // MARK: Tests
-  
-  func testSomething()
-  {
-    // Given
+class ___VARIABLE_sceneName___WorkerTests: XCTestCase {
     
-    // When
+    // MARK: Subject under test
+    var sut: ___VARIABLE_sceneName___Worker!
     
-    // Then
-  }
+    // MARK: Test lifecycle
+    override func setUp() {
+        super.setUp()
+        setup___VARIABLE_sceneName___Worker()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    // MARK: Test setup
+    func setup___VARIABLE_sceneName___Worker() {
+        sut = ___VARIABLE_sceneName___Worker()
+    }
+    
+    // MARK: Test doubles
+    
+    // MARK: Tests
+    func testSomething() {
+        // Given
+        
+        // When
+        
+        // Then
+    }
+    
 }
