9d23629c013d74ea72e90e7c2072a7e520414ee2
Yağızhan Akduman <yagizhanakduman@gmail.com>
2023-01-03 20:23:29 +0300

    presentation logic spy

---
+++ CleanSwift/Unit Tests.xctemplate/Presenter/___FILEBASENAME___PresentationLogicSpy.swift
@@ -0,0 +1,13 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+@testable import ___PROJECTNAMEASIDENTIFIER___
+
+final class ___VARIABLE_sceneName___PresentationLogicSpy: ___VARIABLE_sceneName___PresentationLogic {
+    
+}
