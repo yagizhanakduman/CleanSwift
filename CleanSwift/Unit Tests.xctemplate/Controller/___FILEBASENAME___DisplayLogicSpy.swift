4e8a8451c108c006d9790df04a116ea5888c9fe3
Yağızhan Akduman <yagizhanakduman@gmail.com>
2023-01-03 20:21:42 +0300

    display logic spy & routing logic spy

---
+++ CleanSwift/Unit Tests.xctemplate/Controller/___FILEBASENAME___DisplayLogicSpy.swift
@@ -0,0 +1,13 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+@testable import ___PROJECTNAMEASIDENTIFIER___
+
+final class ___VARIABLE_sceneName___DisplayLogicSpy: ___VARIABLE_sceneName___DisplayLogic {
+    
+}
---
+++ CleanSwift/Unit Tests.xctemplate/Controller/___FILEBASENAME___RoutingLogicSpy.swift
@@ -0,0 +1,13 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+@testable import ___PROJECTNAMEASIDENTIFIER___
+
+final class ___VARIABLE_sceneName___RoutingLogicSpy: NSObject, ___VARIABLE_sceneName___RoutingLogic, ___VARIABLE_sceneName___DataPassing {
+    var dataStore: ___VARIABLE_sceneName___DataStore?
+}
